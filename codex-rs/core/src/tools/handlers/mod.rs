yukimaru77/codex

env_switch: switch to item-level environment targeting (no turn interrupt)
The previous design swapped the thread's sticky environment and forced
a self-interrupt + deferred continuation to make the switch take effect,
because a turn's environment is frozen at turn start. That interrupt
collides with the /goal pursuit loop (and the headless exec loop): the
turn is interrupted but the continuation never resumes, so work stalls.

Reworked env_switch to be side-effect-free and per-tool-call instead:

- env_switch now only provisions + registers the remote codex as a named
  environment (docker:<c> / ssh:<host>) and returns its id to the model.
  It does NOT interrupt the turn, swap the default, or schedule a
  continuation. Removed schedule_continuation, set_thread_environments,
  and the TurnContext.continuation_scheduled flag entirely.
- Tools (shell/exec_command/apply_patch/view_image) carry environment_id
  whenever the env_switch feature is on, so the model targets a specific
  environment per call. resolve_tool_environment now falls back to a live
  EnvironmentManager lookup (with cwd recorded per environment in
  SessionServices.dynamic_environment_cwds) so a dynamically registered
  environment is usable in the same turn.
- Omitting environment_id keeps running on the local host. A single turn
  can therefore mix host and container work, and a sub-agent can switch
  in isolation without affecting main.

Verified in the codex TUI: /goal pursuit + switch now completes (no
'Conversation interrupted'), host+container mix in one turn, sub-agent
isolated switch, all on a freshly provisioned container.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

codex-rs/core/src/tools/handlers/mod.rs
@@ -151,25 +151,61 @@ fn resolve_workdir_base_path(
         .map_or_else(|| default_cwd.clone(), |workdir| default_cwd.join(workdir)))
 }
 
-fn resolve_tool_environment<'a>(
-    turn: &'a TurnContext,
+/// Resolve the environment to use for a tool call.
+///
+/// Resolution order:
+/// 1. `environment_id` is `None` → return the primary turn environment (borrowed from `turn`).
+/// 2. `environment_id` is `Some(id)` and `id` is in `turn.environments.turn_environments` →
+///    clone and return it (same as before).
+/// 3. `environment_id` is `Some(id)`, not in `turn` but present in the live
+///    `EnvironmentManager` → synthesize a `TurnEnvironment` using the cwd recorded in
+///    `session.services.dynamic_environment_cwds` (or fall back to the primary cwd / "/").
+/// 4. Otherwise → "unknown turn environment id" error.
+///
+/// The function now returns an owned `TurnEnvironment` rather than a borrow because synthesized
+/// values in case 3 have no backing storage in `turn`.
+pub(crate) async fn resolve_tool_environment(
+    session: &Session,
+    turn: &TurnContext,
     environment_id: Option<&str>,
-) -> Result<Option<&'a TurnEnvironment>, FunctionCallError> {
-    environment_id.map_or_else(
-        || Ok(turn.environments.primary()),
-        |environment_id| {
-            turn.environments
-                .turn_environments
-                .iter()
-                .find(|environment| environment.environment_id == environment_id)
-                .map(Some)
-                .ok_or_else(|| {
-                    FunctionCallError::RespondToModel(format!(
-                        "unknown turn environment id `{environment_id}`"
-                    ))
-                })
-        },
-    )
+) -> Result<Option<TurnEnvironment>, FunctionCallError> {
+    let Some(env_id) = environment_id else {
+        return Ok(turn.environments.primary().cloned());
+    };
+
+    // Fast path: id already in the frozen turn list.
+    if let Some(found) = turn
+        .environments
+        .turn_environments
+        .iter()
+        .find(|e| e.environment_id == env_id)
+    {
+        return Ok(Some(found.clone()));
+    }
+
+    // Live fallback: look up through EnvironmentManager (for dynamically
+    // registered environments, e.g. registered by env_switch in the same turn).
+    if let Some(environment) = session.services.environment_manager.get_environment(env_id) {
+        let cwd = {
+            let cwds = session.services.dynamic_environment_cwds.lock().await;
+            cwds.get(env_id).cloned()
+        }
+        .or_else(|| turn.environments.primary().map(|p| p.cwd.clone()))
+        .unwrap_or_else(|| {
+            AbsolutePathBuf::from_absolute_path(std::path::Path::new("/"))
+                .expect("/ is always absolute")
+        });
+        return Ok(Some(TurnEnvironment {
+            environment_id: env_id.to_string(),
+            environment,
+            cwd,
+            shell: None,
+        }));
+    }
+
+    Err(FunctionCallError::RespondToModel(format!(
+        "unknown turn environment id `{env_id}`"
+    )))
 }
 
 /// Validates feature/policy constraints for `with_additional_permissions` and

yukimaru77/codex-switch

codex-rs/core/src/tools/handlers/mod.rs
@@ -155,24 +155,69 @@ fn resolve_workdir_base_path(
         .map_or_else(|| default_cwd.clone(), |workdir| default_cwd.join(workdir)))
 }
 
-fn resolve_tool_environment<'a>(
-    environments: &'a TurnEnvironmentSnapshot,
+/// Resolve the environment to use for a tool call.
+///
+/// Resolution order:
+/// 1. `environment_id` is `None` → return the primary turn environment (borrowed from `turn`).
+/// 2. `environment_id` is `Some(id)` and `id` is in `turn.environments.turn_environments` →
+///    clone and return it (same as before).
+/// 3. `environment_id` is `Some(id)`, not in `turn` but present in the live
+///    `EnvironmentManager` → synthesize a `TurnEnvironment` using the cwd recorded in
+///    `session.services.dynamic_environment_cwds` (or fall back to the primary cwd / "/").
+/// 4. Otherwise → "unknown turn environment id" error.
+///
+/// The function now returns an owned `TurnEnvironment` rather than a borrow because synthesized
+/// values in case 3 have no backing storage in `turn`.
+pub(crate) async fn resolve_tool_environment(
+    session: &Session,
+    turn: &TurnContext,
     environment_id: Option<&str>,
-) -> Result<Option<&'a TurnEnvironment>, FunctionCallError> {
-    environment_id.map_or_else(
-        || Ok(environments.primary()),
-        |environment_id| {
-            environments
-                .turn_environments()
-                .find(|environment| environment.environment_id == environment_id)
-                .map(Some)
-                .ok_or_else(|| {
-                    FunctionCallError::RespondToModel(format!(
-                        "unknown turn environment id `{environment_id}`"
-                    ))
-                })
-        },
-    )
+) -> Result<Option<TurnEnvironment>, FunctionCallError> {
+    let Some(env_id) = environment_id else {
+        return Ok(turn.environments.primary().cloned());
+    };
+
+    // Fast path: id already in the frozen turn list.
+    if let Some(found) = turn
+        .environments
+        .turn_environments()
+        .find(|environment| environment.environment_id == env_id)
+    {
+        return Ok(Some(found.clone()));
+    }
+
+    // Live fallback: look up through EnvironmentManager (for dynamically
+    // registered environments, e.g. registered by env_switch in the same turn).
+    if let Some(environment) = session.services.environment_manager.get_environment(env_id) {
+        let cwd = {
+            let cwds = session.services.dynamic_environment_cwds.lock().await;
+            cwds.get(env_id).cloned()
+        }
+        .or_else(|| {
+            turn.environments
+                .primary()
+                .and_then(|primary| primary.cwd().to_abs_path())
+        })
+        .unwrap_or_else(|| {
+            AbsolutePathBuf::from_absolute_path(std::path::Path::new("/"))
+                .expect("/ is always absolute")
+        });
+        let workspace_roots = turn
+            .environments
+            .primary()
+            .map_or_else(Vec::new, |primary| primary.workspace_roots().to_vec());
+        return Ok(Some(TurnEnvironment::new(
+            env_id.to_string(),
+            environment,
+            codex_utils_path_uri::PathUri::from_abs_path(&cwd),
+            workspace_roots,
+            None,
+        )));
+    }
+
+    Err(FunctionCallError::RespondToModel(format!(
+        "unknown turn environment id `{env_id}`"
+    )))
 }
 
 /// Validates feature/policy constraints for `with_additional_permissions` and